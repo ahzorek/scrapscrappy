1cfec64d7a45a7a564c5667e3998ad2f51573d2a
Andre <andrezorek@gmail.com>
2024-02-03 23:45:54 -0300

    melhorias com tamanhos de embalagem, imagens e capturando tipo da cervaja

--- scraper_ambev_bulk.js
+++ scraper_ambev_bulk.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 const productsInfoArray = []
 const failedUrls = []
 
-async function getHTML(url, retryCount = 2, delay = 500) {
+async function getHTML(url, retryCount = 2, delay = 50) {
   console.log('iniciando tentativa em:::', url)
   try {
     await new Promise(resolve => setTimeout(resolve, delay))
@@ -42,25 +42,36 @@ async function processHTML(urls) {
       const $ = cheerio.load(html)
 
       const productInfo = {
-        nomeProduto: $('h1.coh-heading').text().trim(),
-        tamanhoEmbalagem: $('.js-product-nav.product-size.is-active').first().text().trim(),
-        descricaoProduto: $('.product-description div').first().text().trim(),
+        nomeCerveja: $('h1.coh-heading').text().trim(),
+        descriCerveja: $('.product-description div').first().text().trim(),
         valorIBU: $('.product-ibu-info .properties-value-top').first().text().trim(),
         teorAlcoolico: $('.product-alcoholic-info .properties-description-top').first().text().trim().split('%')[0],
-        classificacaoCorpo: $('.product-body-info .properties-value-top').first().text().trim(),
-        temperaturaIdeal: $('.product-ideal-temperature-info .properties-description-top').first().text().trim(),
+        corpo: $('.product-body-info .properties-value-top').first().text().trim(),
+        tempIdeal: $('.product-ideal-temperature-info .properties-description-top').first().text().trim(),
         ingredientes: [],
-        informacaoAlergenica: $('.coh-wysiwyg span').last().text().trim(),
-        imagemProduto: `https://www.ambev.com.br${$('.product-image img').attr('src')}`
+        informacaoAlergen: $('.coh-wysiwyg span').last().text().trim(),
+        tipoCerveja: $('.product-style-info h3.properties-description-top').first().text().trim()
       }
 
+      productInfo.imagensProduto = []
+      $('.product-image img').each((index, element) => {
+        const imageUrl = $(element).attr('src');
+        productInfo.imagensProduto.push(`https://www.ambev.com.br${imageUrl}`)
+      })
+
+      const tamanhosEmbalagemSet = new Set()
+
+      $('.product-size').each((index, element) => {
+        tamanhosEmbalagemSet.add($(element).first().text().trim())
+      });
+
+      productInfo.tamanhosEmbalagem = [...tamanhosEmbalagemSet]
 
       $('.section-product_ingredients h3').each((index, element) => {
         productInfo.ingredientes.push($(element).text().trim())
       })
 
       const nutrientInfo = []
-
       $('.nutrient-content-item').each((index, element) => {
         const nutrientItem = {
           nutrientName: $(element).find('.properties-subtitle-bottom').text().trim(),
@@ -72,18 +83,18 @@ async function processHTML(urls) {
       productInfo.informacoesNutricionais = nutrientInfo
 
       const harmonizationInfo = []
-
       $('.section-product_good_with h3').each((index, element) => {
         harmonizationInfo.push($(element).text().trim())
       })
 
       productInfo.harmonizacoes = harmonizationInfo
 
+      //salva a cervej no arrayzao
       productsInfoArray.push(productInfo)
     }
   }
 
-  console.log('Informações scrapeadas:', productsInfoArray)
+  console.log('Informações scrapeadas:', productsInfoArray.length, 'cervejas puxadas.')
 
   saveAsJSON(productsInfoArray, failedUrls)
 }
