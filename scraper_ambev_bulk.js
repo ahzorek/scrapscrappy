5abf6c66b63620b052c73744f77a10f7b3fc62a9
Andre <andrezorek@gmail.com>
2024-02-03 23:20:14 -0300

    fix manual nas urls com redirect. alteração no seletor das harmonizações corrige valores de ingredientes duplicados

--- scraper_ambev_bulk.js
+++ scraper_ambev_bulk.js
@@ -73,9 +73,8 @@ async function processHTML(urls) {
 
       const harmonizationInfo = []
 
-      $('.term-list-item').each((index, element) => {
-        const harmonizationItem = $(element).find('.properties-subtitle-bottom').text().trim()
-        harmonizationInfo.push(harmonizationItem);
+      $('.section-product_good_with h3').each((index, element) => {
+        harmonizationInfo.push($(element).text().trim())
       })
 
       productInfo.harmonizacoes = harmonizationInfo
@@ -89,40 +88,6 @@ async function processHTML(urls) {
   saveAsJSON(productsInfoArray, failedUrls)
 }
 
-const urls = [
-  'https://www.ambev.com.br/marcas/cervejas/adriatica',
-  'https://www.ambev.com.br/marcas/cervejas/andes',
-  'https://www.ambev.com.br/marcas/cervejas/antarctica',
-  'https://www.ambev.com.br/marcas/cervejas/becks',
-  'https://www.ambev.com.br/marcas/cervejas/berrio-do-piaui',
-  'https://www.ambev.com.br/marcas/cervejas/bohemia-puro-malte',
-  'https://www.ambev.com.br/marcas/cervejas/brahma-chopp',
-  'https://www.ambev.com.br/marcas/cervejas/budweiser',
-  'https://www.ambev.com.br/marcas/cervejas/caracu',
-  'https://www.ambev.com.br/marcas/cervejas/colorado-appia',
-  'https://www.ambev.com.br/marcas/cervejas/corona',
-  'https://www.ambev.com.br/marcas/cervejas/esmera-do-goias',
-  'https://www.ambev.com.br/marcas/cervejas/franziskaner-hell',
-  'https://www.ambev.com.br/marcas/cervejas/goose-island-ipa',
-  'https://www.ambev.com.br/marcas/cervejas/hoegaarden-wit-blanche',
-  'https://www.ambev.com.br/marcas/cervejas/kona-big-wave',
-  'https://www.ambev.com.br/marcas/cervejas/leffe-blonde',
-  'https://www.ambev.com.br/marcas/cervejas/legitima',
-  'https://www.ambev.com.br/marcas/cervejas/magnifica',
-  'https://www.ambev.com.br/marcas/cervejas/modelo',
-  'https://www.ambev.com.br/marcas/cervejas/nossa-de-pernambuco',
-  'https://www.ambev.com.br/marcas/cervejas/antarctica-original',
-  'https://www.ambev.com.br/marcas/cervejas/patagonia-bohemian-pilsener',
-  'https://www.ambev.com.br/marcas/cervejas/polar',
-  'https://www.ambev.com.br/marcas/cervejas/polar-puro-malte-gaucho',
-  'https://www.ambev.com.br/marcas/cervejas/serramalte',
-  'https://www.ambev.com.br/marcas/cervejas/serrana',
-  'https://www.ambev.com.br/marcas/cervejas/michelob-ultra',
-  'https://www.ambev.com.br/marcas/cervejas/spaten',
-  'https://www.ambev.com.br/marcas/cervejas/stella-artois',
-  'https://www.ambev.com.br/marcas/cervejas/tres-fidalgas',
-  'https://www.ambev.com.br/marcas/cervejas/wals-42',
-  'https://www.ambev.com.br/marcas/cervejas/abiuda-de-sergipe'
-]
+const urls = require('./urls.js')
 
 processHTML(urls)
